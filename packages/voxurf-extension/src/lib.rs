7fdc31d50e25a2320406d3fb0aa6352443c024b2
Michael Stephen Lape <z5477893@ad.unsw.edu.au>
2024-03-02 17:38:29 +1100

    completed primitive UI waiting for animation idk

--- packages/voxurf-extension/src/lib.rs
+++ packages/voxurf-extension/src/lib.rs
@@ -1,23 +1,6 @@
 use sycamore::prelude::*;
 use wasm_bindgen::prelude::*;
 
-// Code State
-// enum State {
-//     Listening,
-//     Processing,
-//     Available,
-// }
-
-// impl Display for State {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         match self {
-//             State::Listening => write!(f, "Listening"),
-//             State::Processing => write!(f, "Processing"),
-//             State::Available => write!(f, "Record"),
-//         }
-//     }
-// }
-
 #[wasm_bindgen]
 pub fn main() {
     sycamore::render(|cx| {
@@ -27,10 +10,15 @@ pub fn main() {
 
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
-    // let interactive = create_memo(cx, || state.get() == State::Available);
     view! {
         cx,
-        textarea(readonly=true)
-        button(style="width:100%") { "RECORD"}
+        div(class="row-arrange") {
+            div(class="left") {
+                p(id="transcription") { "Did I hear you right?" }
+            }
+            div(class="right") {
+                button() { img(src="assets/zrolatency_logo_red_back.png")}
+            }
+        }
     }
 }
