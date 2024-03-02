ed840f2345aa85f5bbd65c07c07cd06075272f2d
Michael Stephen Lape <z5477893@ad.unsw.edu.au>
2024-03-03 00:00:22 +1100

    implemented state scaffolding

--- packages/voxurf-extension/src/lib.rs
+++ packages/voxurf-extension/src/lib.rs
@@ -1,5 +1,4 @@
 use sycamore::{component, prelude::*, view};
-use sycamore_router::{HistoryIntegration, Route, Router};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,16 +8,10 @@ pub fn main() {
     });
 }
 
-#[derive(Route)]
 enum AppState {
-    #[to("/")]
     Listening,
-    #[to("/process")]
     Processing,
-    #[to("/free")]
     Available,
-    #[not_found]
-    NotFound,
 }
 
 #[component]
@@ -26,26 +19,33 @@ fn App<G: Html>(cx: Scope) -> View<G> {
     let app_state = create_signal(cx, AppState::Listening);
     view! { cx,
         div(class="row-arrange") {
-            Router(
-                integration=HistoryIntegration::new(),
-                view=|cx, route: &ReadSignal<AppState>| {
-                    view! { cx,
-                        (match route.get().as_ref() {
-                            // TODO: Implement States
-                            AppState::Available => view! { cx,
-                            },
-                            AppState::Listening => view! { cx,
-                            },
-                            AppState::Processing=> view! { cx,
-                            },
-                            AppState::NotFound=> view! { cx,
-                            }
-                        })
-                    }
-                }
-            )
             div(class="left") {
                 p(id="transcription") { "Did I hear you right?" }
+            }
+            div(class="right") {
+                DynamicButton(app_state)
+            }
+        }
+    }
+}
+
+#[component]
+fn DynamicButton<G: Html>(cx: Scope, state: &ReadSignal<AppState>) -> View<G> {
+    match &*state.get() {
+        // TODO: Apply UI in each state.
+        AppState::Listening => {
+            view! { cx,
+                p { "ListenMode "}
+            }
+        }
+        AppState::Processing => {
+            view! { cx,
+                p { "ProcessMode "}
+            }
+        }
+        AppState::Available => {
+            view! { cx,
+                p { "AvailMode "}
             }
         }
     }
