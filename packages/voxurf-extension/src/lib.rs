349a2d4c2d2195bcddc85ee1845294a977971abf
Michael Stephen Lape <z5477893@ad.unsw.edu.au>
2024-03-02 11:37:15 +1100

    tested sycamore other stuff idk

--- packages/voxurf-extension/src/lib.rs
+++ packages/voxurf-extension/src/lib.rs
@@ -5,9 +5,15 @@ use voxurf::get_message;
 
 #[wasm_bindgen]
 pub fn main() {
+    // vars to be loaded for script starts
+    let env_vars = 
+
+    // show extension
     sycamore::render(|cx| {
         view! { cx, App() }
-    })
+    });
+
+    // save vars 
 }
 
 #[component]
@@ -15,5 +21,6 @@ fn App<G: Html>(cx: Scope) -> View<G> {
     view! {
         cx,
         p { (get_message()) }
+        button(on:click=|_| { }) { "THE BUTTON" }
     }
 }
